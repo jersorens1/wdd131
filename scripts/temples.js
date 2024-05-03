b6e768c5e533d95db65c8603b0efdc0430dbde9c
Jeremy Sorensen <jersorens1@byui.edu>
2024-05-03 10:25:33 -0600

    launch

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,13 @@
+// toggles hamburger menu icon to navigation list and displays close icon
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation')
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('show')
+    hamButton.classList.toggle('show')
+})
+
+// add get dates and last modified from index.html to temples page
 let currentDate = new Date();
 let currentYear = currentDate.getFullYear();
 let currentYearElement = document.getElementById("currentyear");
